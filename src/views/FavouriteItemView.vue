99e81c2b7c7e969e0916faa5639f9373e3d09ef3
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-09 14:38:18 +0200

    feat: create favourite item page

---
+++ src/views/FavouriteItemView.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: 'FavouritesItemView',
+}
+</script>
+
+<template>
+  <div class="favourites-item-view">
+    <h1>This is a favourites item page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .favourites-item-view {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
