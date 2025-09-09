5338e71eded5efe7f1d37ff5d60fdd8ccbbf177a
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-09 19:09:19 +0200

    feat: update popular item page

---
+++ src/views/PopularItemView.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: 'PopularItemView',
+}
+</script>
+
+<template>
+  <div class="popular-item-view">
+    <h1>This is a popular item page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .popular-item-view {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
