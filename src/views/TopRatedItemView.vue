b8f91dec64e11b5992dbd8952a76c5e2aa3b72d5
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-09 19:08:50 +0200

    feat: update top rated item page

---
+++ src/views/TopRatedItemView.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: 'RatedItemView',
+}
+</script>
+
+<template>
+  <div class="rated-item-view">
+    <h1>This is a rated item page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .rated-item-view {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
