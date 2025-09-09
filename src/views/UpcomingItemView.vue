d381e1c0c40bb4d0287238b298c5b45bb9858266
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-09 19:07:51 +0200

    feat: add upcoming item page

---
+++ src/views/UpcomingItemView.vue
@@ -0,0 +1,25 @@
+<script>
+import { RouterView } from 'vue-router'
+
+export default {
+  name: 'UpcomingItemView',
+}
+</script>
+
+<template>
+  <div class="upcoming-item">
+    <h1>This is an upcoming item page</h1>
+  </div>
+
+  <RouterView />
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .upcoming-item {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
