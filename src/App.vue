53de60278c85c3c1d93ea558c7fea94bdb4be0af
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-09 20:16:50 +0200

    feat: create sidebar with routing

--- src/App.vue
+++ src/App.vue
@@ -4,92 +4,51 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <v-app>
+    <v-layout>
+      <v-navigation-drawer expand-on-hover permanent rail>
+        <v-list>
+          <v-list-item
+            prepend-icon="mdi-play-circle-outline"
+            subtitle="Home"
+            title="Home"
+            to="/"
+            router
+          ></v-list-item>
+        </v-list>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+        <v-divider></v-divider>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/popular">Popular</RouterLink>
-        <RouterLink to="/top-rated">Top Rated</RouterLink>
-        <RouterLink to="/upcoming">Upcoming</RouterLink>
-      </nav>
-    </div>
+        <v-list density="compact" nav>
+          <v-list-item
+            prepend-icon="mdi-fire"
+            title="Popular"
+            value="popular"
+            to="/popular"
+            router
+          ></v-list-item>
+          <v-list-item
+            prepend-icon="mdi-star"
+            title="Top Rated"
+            value="top-rated"
+            to="/top-rated"
+            router
+          ></v-list-item>
+          <v-list-item
+            prepend-icon="mdi-calendar"
+            title="Upcoming"
+            value="upcoming"
+            to="/upcoming"
+            router
+          ></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
 
-    <!-- <v-app>
       <v-main>
-        <v-container>
-          <v-btn color="primary">Hello Vuetify</v-btn>
-        </v-container>
+        <RouterView />
       </v-main>
-    </v-app> -->
-  </header>
-
-  <RouterView />
+    </v-layout>
+  </v-app>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
