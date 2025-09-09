8c4cb5852af178b1ee2ed09b64f3c1709e4208a4
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-09 20:19:52 +0200

    feat: create root view page

---
+++ src/views/RootView.vue
@@ -0,0 +1,23 @@
+<script>
+import { RouterView } from 'vue-router'
+
+export default {
+  name: 'RootView',
+}
+</script>
+
+<template>
+  <div class="not-found">
+    <RouterView />
+  </div>
+</template>
+
+<style>
+/* @media (min-width: 1024px) {
+  .not-found {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+} */
+</style>
