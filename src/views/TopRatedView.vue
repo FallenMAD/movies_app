198cebe415e69747d326d2cb5ca970ecd4d1ff90
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-09 19:08:36 +0200

    feat: update top rated page

---
+++ src/views/TopRatedView.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: 'RatedView',
+}
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is a rated page</h1>
+  </div>
+  <RouterView />
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
