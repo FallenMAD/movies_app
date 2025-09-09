c482a74434af40cc6c307288ed9920035d842440
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-09 19:07:33 +0200

    feat: add upcoming page

---
+++ src/views/UpcomingView.vue
@@ -0,0 +1,25 @@
+<script>
+import { RouterView } from 'vue-router'
+
+export default {
+  name: 'UpcomingView',
+}
+</script>
+
+<template>
+  <div class="upcoming">
+    <h1>This is an upcoming page</h1>
+  </div>
+
+  <RouterView />
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .upcoming {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
