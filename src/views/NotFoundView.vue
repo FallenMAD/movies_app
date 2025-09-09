b9ffdedb8b6b2e35d8d98fb88b11636b8442a199
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-09 19:15:16 +0200

    feat: add not found page

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: 'NotFoundView',
+}
+</script>
+
+<template>
+  <div class="not-found">
+    <h1>This is a not found page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .not-found {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
