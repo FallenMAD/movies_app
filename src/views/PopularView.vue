cc1f172b6ac68e1cde424bb3e5501403e7753aac
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-09 19:09:31 +0200

    feat: update popular page

---
+++ src/views/PopularView.vue
@@ -0,0 +1,25 @@
+<script>
+import { RouterView } from 'vue-router'
+
+export default {
+  name: 'PopularView',
+}
+</script>
+
+<template>
+  <div class="popular">
+    <h1>This is a popular page</h1>
+  </div>
+
+  <RouterView />
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .popular {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
