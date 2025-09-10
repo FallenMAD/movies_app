f2f676f6e12d311867ad4ed1af4b7b0ced381e67
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-10 11:08:09 +0200

    refactor(SideBar): use appRoutes, wrap component in defineComponent for better typing

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,38 +1,27 @@
+<script>
+import { defineComponent } from 'vue'
+import { appRoutes } from '../utils/appRoutes'
+
+export default defineComponent({
+  name: 'SideBar',
+  data() {
+    return {
+      routes: appRoutes,
+    }
+  },
+})
+</script>
+
 <template>
   <v-navigation-drawer expand-on-hover permanent rail>
-    <v-list>
-      <v-list-item
-        prepend-icon="mdi-play-circle-outline"
-        subtitle="Home"
-        title="Home"
-        to="/"
-        exact
-      ></v-list-item>
-    </v-list>
-
-    <v-divider></v-divider>
-
     <v-list density="compact" nav>
       <v-list-item
-        prepend-icon="mdi-fire"
-        title="Popular"
-        value="popular"
-        to="/popular"
-        :exact="true"
-      ></v-list-item>
-      <v-list-item
-        prepend-icon="mdi-star"
-        title="Top Rated"
-        value="top-rated"
-        to="/top-rated"
-        :exact="true"
-      ></v-list-item>
-      <v-list-item
-        prepend-icon="mdi-calendar"
-        title="Upcoming"
-        value="upcoming"
-        to="/upcoming"
-        :exact="true"
+        v-for="route in routes"
+        :key="route.path"
+        :prepend-icon="route.icon"
+        :subtitle="route.subtitle"
+        :title="route.title"
+        :to="route.path"
       ></v-list-item>
     </v-list>
   </v-navigation-drawer>
