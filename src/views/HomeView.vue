e33260da99939de65fc4579e8d5bbc215ef431ba
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-10 12:01:52 +0200

    styles(HomeView): add title and styles for it

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,4 +6,13 @@ export default defineComponent({
 })
 </script>
 
-<template></template>
+<template>
+  <v-container class="my-5">
+    <v-row>
+      <v-col class="d-flex justify-center align-center flex-column">
+        <h1 class="text-h4 font-weight-bold mb-2">Home Movies</h1>
+        <p class="text-subtitle-1 text--secondary">Here you can see all playing now movies.</p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
