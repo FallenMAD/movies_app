c5f239cb02df44cb48eb044575c9ff4259142117
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-10 12:41:37 +0200

    refactor(NotFoundView): add ViewTitle component to render title

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -7,11 +7,5 @@ export default defineComponent({
 </script>
 
 <template>
-  <v-container class="my-5">
-    <v-row>
-      <v-col class="d-flex justify-center align-center flex-column">
-        <h1 class="text-h4 font-weight-bold mb-2">Not Found Page</h1>
-      </v-col>
-    </v-row>
-  </v-container>
+  <ViewTitle title="Not Found Page" />
 </template>
