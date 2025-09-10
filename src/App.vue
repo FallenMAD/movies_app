242ac9557bd0116c0d26f10ee1762aeaa40810a5
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-10 09:50:18 +0200

    feat: add side bar component

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,13 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import SideBar from './components/SideBar.vue'
 </script>
 
 <template>
   <v-app>
     <v-layout>
-      <v-navigation-drawer expand-on-hover permanent rail>
-        <v-list>
-          <v-list-item
-            prepend-icon="mdi-play-circle-outline"
-            subtitle="Home"
-            title="Home"
-            to="/"
-            router
-          ></v-list-item>
-        </v-list>
-
-        <v-divider></v-divider>
-
-        <v-list density="compact" nav>
-          <v-list-item
-            prepend-icon="mdi-fire"
-            title="Popular"
-            value="popular"
-            to="/popular"
-            router
-          ></v-list-item>
-          <v-list-item
-            prepend-icon="mdi-star"
-            title="Top Rated"
-            value="top-rated"
-            to="/top-rated"
-            router
-          ></v-list-item>
-          <v-list-item
-            prepend-icon="mdi-calendar"
-            title="Upcoming"
-            value="upcoming"
-            to="/upcoming"
-            router
-          ></v-list-item>
-        </v-list>
-      </v-navigation-drawer>
+      <SideBar />
 
       <v-main>
         <RouterView />
