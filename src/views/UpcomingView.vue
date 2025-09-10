c3b339fb9cc6bb93e80b5e444c0893276b519e41
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-10 12:31:19 +0200

    refactor(UpcomingView): add ViewTitle component to render title and subtitle

--- src/views/UpcomingView.vue
+++ src/views/UpcomingView.vue
@@ -7,12 +7,5 @@ export default defineComponent({
 </script>
 
 <template>
-  <v-container class="my-5">
-    <v-row>
-      <v-col class="d-flex justify-center align-center flex-column">
-        <h1 class="text-h4 font-weight-bold mb-2">Upcoming Movies</h1>
-        <p class="text-subtitle-1 text--secondary">Here you can see all upcoming releases.</p>
-      </v-col>
-    </v-row>
-  </v-container>
+  <ViewTitle title="Upcoming Movies" subtitle="Here you can see all upcoming releases." />
 </template>
