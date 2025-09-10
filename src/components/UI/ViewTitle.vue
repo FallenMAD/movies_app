fd2f33aaf1cdf1c379545cdf61565555382488eb
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-10 12:27:57 +0200

    feat(ViewTitle): create reusable component for title and subtitle

---
+++ src/components/UI/ViewTitle.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+import type { PropType } from 'vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'ViewTitle',
+  props: {
+    title: {
+      type: String as PropType<string | undefined>,
+      required: true,
+    },
+    subtitle: {
+      type: String as PropType<string | undefined>,
+    },
+  },
+})
+</script>
+
+<template>
+  <v-container class="my-5">
+    <v-row>
+      <v-col class="d-flex justify-center align-center flex-column">
+        <h1 class="text-h4 font-weight-bold mb-2">{{ title || 'Title' }}</h1>
+        <p v-if="subtitle" class="text-subtitle-1 text--secondary">{{ subtitle || 'Subtitle' }}</p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
