892129cb12ae58337c53bfac32fe6eb9ea0d1ca2
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-10 12:36:39 +0200

    refactor(TopRatedView): add ViewTitle component to render title and subtitle

--- src/views/TopRatedView.vue
+++ src/views/TopRatedView.vue
@@ -7,12 +7,5 @@ export default defineComponent({
 </script>
 
 <template>
-  <v-container class="my-5">
-    <v-row>
-      <v-col class="d-flex justify-center align-center flex-column">
-        <h1 class="text-h4 font-weight-bold mb-2">Top Rated Movies</h1>
-        <p class="text-subtitle-1 text--secondary">Here you can see all top rated movies.</p>
-      </v-col>
-    </v-row>
-  </v-container>
+  <ViewTitle title="Top Rated Movies" subtitle="Here you can see all top rated movies." />
 </template>
