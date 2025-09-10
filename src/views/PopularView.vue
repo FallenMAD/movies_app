734a1ac40fa36f837e347c876f8fa2628497d2f2
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-10 12:40:17 +0200

    refactor(PopularView):add ViewTitle component to render title and subtitle

--- src/views/PopularView.vue
+++ src/views/PopularView.vue
@@ -7,12 +7,5 @@ export default defineComponent({
 </script>
 
 <template>
-  <v-container class="my-5">
-    <v-row>
-      <v-col class="d-flex justify-center align-center flex-column">
-        <h1 class="text-h4 font-weight-bold mb-2">Popular Movies</h1>
-        <p class="text-subtitle-1 text--secondary">Here you can see all popular releases.</p>
-      </v-col>
-    </v-row>
-  </v-container>
+  <ViewTitle title="Popular Movies" subtitle="Here you can see all popular releases." />
 </template>
