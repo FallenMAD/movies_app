e82ecec5a391b2433725263d0d80c05e62ced410
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-10 20:07:31 +0200

    feat: add new logic and make api request

--- src/views/PopularView.vue
+++ src/views/PopularView.vue
@@ -1,8 +1,23 @@
-<script>
+<script lang="ts">
 import { defineComponent } from 'vue'
+import { usePopularMovieStore } from '@/stores/popularMovie'
 
 export default defineComponent({
   name: 'PopularView',
+  data() {
+    return {
+      store: usePopularMovieStore(),
+    }
+  },
+  computed: {
+    popularMovies() {
+      return this.store.movies
+    },
+  },
+  mounted() {
+    this.store.fetchPopularMovies(1)
+    this.popularMovies
+  },
 })
 </script>
 
