c743db5e22436dbf74b45fa64de20cb8e96c9b88
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-10 12:01:03 +0200

    refactor(HomeView): wrap in defineComponent & delete unnecessary imports

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,9 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HomeView',
+})
 </script>
 
 <template></template>
