6e15b7b7ebaadfb606d64b169ae9a77fe20fe50d
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-10 12:35:31 +0200

    refactor(UpcomingItemView): add ViewTitle component to render title

--- src/views/UpcomingItemView.vue
+++ src/views/UpcomingItemView.vue
@@ -7,11 +7,5 @@ export default defineComponent({
 </script>
 
 <template>
-  <v-container class="my-5">
-    <v-row>
-      <v-col class="d-flex justify-center align-center flex-column">
-        <h1 class="text-h6 font-weight-bold mb-2">Upcoming Movie Item</h1>
-      </v-col>
-    </v-row>
-  </v-container>
+  <ViewTitle title="Upcoming Movie Item" />
 </template>
