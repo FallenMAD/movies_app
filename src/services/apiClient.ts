e25120a5f657a2fa9064430257ab3eac7e3a47c6
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-11 08:56:54 +0200

    refactor(apiClient): add BASE_URL & important headers

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,9 +1,11 @@
+import { BASE_URL } from '@/utils/BASE_URL'
 import axios from 'axios'
 import type { AxiosInstance } from 'axios'
 
 export const apiClient: AxiosInstance = axios.create({
-  baseURL: '/api',
-  params: {
-    api_key: import.meta.env.VITE_TMDB_TOKEN,
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
   },
 })
