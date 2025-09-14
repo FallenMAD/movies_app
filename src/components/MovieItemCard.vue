a983d02d2e1d79ccc9e16c88bbc5c9cf33004307
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-14 20:26:39 +0200

    feat: create MovieItemCard component to render movie item

---
+++ src/components/MovieItemCard.vue
@@ -0,0 +1,61 @@
+<script lang="ts">
+import type { MovieItem } from '@/types/MovieItem.interface'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'MovieItemCard',
+  props: {
+    movie: {
+      type: Object as () => MovieItem | null,
+      required: true,
+    },
+  },
+})
+</script>
+
+<template>
+  <v-container class="my-6" v-if="movie">
+    <slot name="btn"></slot>
+    <v-row>
+      <v-col cols="12" md="4">
+        <v-img
+          :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`"
+          :alt="movie.title"
+          height="500"
+          class="rounded-lg"
+        />
+      </v-col>
+
+      <v-col cols="12" md="8">
+        <v-card flat class="pa-4">
+          <v-card-title class="text-h4 font-weight-bold px-0">{{ movie.title }}</v-card-title>
+
+          <div class="d-flex flex-wrap mb-2" v-if="movie.genres.length">
+            <v-chip
+              v-for="genre in movie.genres"
+              :key="genre.id"
+              color="primary"
+              text-color="white"
+              class="ma-1"
+              small
+            >
+              {{ genre.name }}
+            </v-chip>
+          </div>
+
+          <div class="d-flex align-center mb-4">
+            <v-icon color="amber darken-2" size="20" class="mr-1">mdi-star</v-icon>
+            <span class="mr-4 font-weight-medium">{{ movie.vote_average.toFixed(1) }}</span>
+
+            <v-icon size="20" class="mr-1">mdi-calendar</v-icon>
+            <span class="font-weight-medium">{{ movie.release_date }}</span>
+          </div>
+
+          <v-card-text class="px-0">
+            {{ movie.overview }}
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
