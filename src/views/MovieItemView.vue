5d6a55cc8a8762a6b17f6e3d1809904539ce6dd1
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-14 15:22:30 +0200

    feat: create movie item page

---
+++ src/views/MovieItemView.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PopularItemView',
+})
+</script>
+
+<template>
+  <ViewTitle title="Popular Movie Item" />
+</template>
