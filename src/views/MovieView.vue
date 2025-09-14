10e7052e3f0d7735e20436d1df083936ef0f8572
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-14 15:22:17 +0200

    feat: create movie page

---
+++ src/views/MovieView.vue
@@ -0,0 +1,36 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useMovieStore } from '@/stores/popularMovie'
+import MoviesList from '@/components/MoviesList.vue'
+import { useRoute } from 'vue-router'
+
+export default defineComponent({
+  name: 'MovieView',
+  components: { MoviesList },
+  data() {
+    return {
+      store: useMovieStore(),
+      route: useRoute(),
+    }
+  },
+  computed: {
+    genreID() {
+      return this.route.params.category as string
+    },
+  },
+  mounted() {
+    this.store.fetchPopularMovies(2, this.genreID)
+    console.log(this.route)
+  },
+  watch: {
+    'route.params.category': function () {
+      this.store.fetchPopularMovies(2, this.genreID)
+    },
+  },
+})
+</script>
+
+<template>
+  <ViewTitle title="Movie List" subtitle="Here you can see all popular releases." />
+  <MoviesList />
+</template>
