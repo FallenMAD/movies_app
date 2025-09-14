7ac8da8a111db7dafa2eee5ec21fa0b380f9afc8
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-14 15:21:12 +0200

    refactor(AppRouterPAths.enum): add new enum & update top_rated path

--- src/types/AppRouterPaths.enum.ts
+++ src/types/AppRouterPaths.enum.ts
@@ -1,7 +1,8 @@
 export enum RoutePaths {
   HOME = '/',
   POPULAR = '/popular',
-  TOP_RATED = '/top-rated',
+  TOP_RATED = '/top_rated',
   UPCOMING = '/upcoming',
   NOT_FOUND = '/:pathMatch(.*)*',
+  CATEGORY = '/:category',
 }
