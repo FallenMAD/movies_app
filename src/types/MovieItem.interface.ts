af6bed6286ece3ad7e9a3d469cc9ac427410dde5
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-15 09:39:23 +0200

    refactor(MovieItem): extends from BaseMovie interface

--- src/types/MovieItem.interface.ts
+++ src/types/MovieItem.interface.ts
@@ -1,27 +1,16 @@
-export interface MovieItem {
-  adult: boolean
-  backdrop_path: string | null
+import type { BaseMovie } from './BaseMovie.interface'
+
+export interface MovieItem extends BaseMovie {
   belongs_to_collection: object | null
   budget: number
   genres: { id: number; name: string }[]
   homepage: string
-  id: number
   imdb_id: string | null
-  original_language: string
-  original_title: string
-  overview: string
-  popularity: number
-  poster_path: string | null
   production_companies: any[]
   production_countries: { iso_3166_1: string; name: string }[]
-  release_date: string
   revenue: number
   runtime: number | null
   spoken_languages: { english_name: string; iso_639_1: string; name: string }[]
   status: string
   tagline: string
-  title: string
-  video: boolean
-  vote_average: number
-  vote_count: number
 }
