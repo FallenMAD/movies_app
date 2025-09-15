ec19e5caf49e92be4fcbc64ef05061a0600a92d2
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-15 09:38:50 +0200

    refactor(Movie.interface): extends from BaseMovie interface

--- src/types/Movie.interface.ts
+++ src/types/Movie.interface.ts
@@ -1,16 +1,5 @@
-export interface Movie {
-  id: number;
-  title: string;
-  original_title: string;
-  original_language: string;
-  overview: string;
-  release_date: string;
-  genre_ids: number[];
-  popularity: number;
-  vote_average: number;
-  vote_count: number;
-  adult: boolean;
-  video: boolean;
-  backdrop_path: string | null;
-  poster_path: string | null;
+import type { BaseMovie } from './BaseMovie.interface'
+
+export interface Movie extends BaseMovie {
+  genre_ids: number[]
 }
