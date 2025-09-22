b84fa0c7e3a6f6a22f6c6700a4ee182312c78f68
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-22 08:38:19 +0200

    feat(global-components): register an Error component

--- src/plugins/global-components.ts
+++ src/plugins/global-components.ts
@@ -1,6 +1,8 @@
 import type { App } from 'vue'
 import ViewTitle from '@/components/UI/ViewTitle.vue'
+import Error from '@/components/Error.vue'
 
 export default function registerGlobalComponents(app: App): void {
   app.component('ViewTitle', ViewTitle)
+  app.component('Error', Error)
 }
