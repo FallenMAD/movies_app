5fb1ccfc319751d264b69ad79875cdfbb3e2b9ea
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-22 08:36:18 +0200

    feat: create an Error component

---
+++ src/components/Error.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Error',
+  props: {
+    error: {
+      type: String,
+      required: false,
+    },
+  },
+})
+</script>
+
+<template>
+  <div class="mx-auto" style="max-width: 600px">
+    <v-alert
+      type="error"
+      elevation="2"
+      border="start"
+      rounded
+      dense
+      class="px-4 py-2 text-sm font-medium text-center"
+    >
+      {{ error || 'error occurred' }}; please try again later.
+    </v-alert>
+  </div>
+</template>
